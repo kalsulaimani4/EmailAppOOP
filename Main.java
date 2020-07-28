7efe242c0d1438e9946a61a1a457b71f69862168
khalil alsulaimani <alsulaimani.2000@outlook.com>
2020-07-29 00:24:30 +0300

    removed customexeption class and set up main class for the main options

--- Main.java
+++ Main.java
@@ -5,5 +5,20 @@ public class Main {
 
         System.out.println("enter number for the service you want : ");
         System.out.println("1- sign up: "+"\n"+"2- sign in: "+"\n"+"3- end program: ");
+        int option = scan.nextInt();
+        int numOfTermination=0;
+
+        while(numOfTermination!=3){
+            switch (option){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("invalid option try again ");
+                    continue;
+            }
+
+        }
     }
 }
