691e1c1c18c73a7035a08db77356b36430ba1366
khalil alsulaimani <alsulaimani.2000@outlook.com>
2020-08-03 12:27:20 +0300

    changed name variable to firstName

--- EmailOwnerInfo.java
+++ EmailOwnerInfo.java
@@ -2,23 +2,22 @@ import java.security.InvalidParameterException;
 import java.util.Vector;
 
 public class EmailOwnerInfo{
-    private String name;
+    private String firstName;
     private String city;
     private int age;
-    private String backQuestion;
-    private String backQuestionAnswer;
 
-    public  EmailOwnerInfo(String name, String city, int age,String backQuestion,String backQuestionAnswer){
-        this.name= name;
+    public  EmailOwnerInfo(String firstName, String city, int age){
+        this.firstName= firstName;
         this.city=city;
         this.age=age;
-        this.backQuestion=backQuestion;
-        this.backQuestionAnswer=backQuestionAnswer;
+
     }
 
     public EmailOwnerInfo(){
-        this("default ","none",0,"default","default");
+        this("default ","none",0);
     }
+
+
 
     public void setAge(int age) {
         this.age = age;
@@ -37,29 +36,10 @@ public class EmailOwnerInfo{
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.firstName = name;
     }
 
     public String getName() {
-        return name;
+        return firstName;
     }
-
-    public void setBackQuestion(String backQuestion) {
-        this.backQuestion = backQuestion;
-    }
-
-    public String getBackQuestion() {
-        return backQuestion;
-    }
-
-    public void setBackQuestionAnswer(String backQuestionAnswer) {
-        this.backQuestionAnswer = backQuestionAnswer;
-    }
-
-    public String getBackQuestionAnswer() {
-        return backQuestionAnswer;
-    }
-
-
-
 }
